67b6e2b02f620e15b8092391ae9c9dbe7a1c14d7
oberon10 <prosperbernrad@gmail.com>
2023-09-22 16:50:48 -0700

    0-create_a_file.pp

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+#This code will create a file name school inside the /tmp directory
+
+# creates a file in /tmp
+
+file { '/tmp/school':
+  content =>'I love Puppet',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+}
